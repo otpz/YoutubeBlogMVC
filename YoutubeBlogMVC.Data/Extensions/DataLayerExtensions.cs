a9f2ee12d2e7f0b2a6dccbfaccae28f021712f06
Osman Topuz <osmantopuz98@gmail.com>
2024-04-19 13:16:43 +0300

    repositories added

---
+++ YoutubeBlogMVC.Data/Extensions/DataLayerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeBlogMVC.Data.Repositories.Abstractions;
+using YoutubeBlogMVC.Data.Repositories.Concretes;
+
+namespace YoutubeBlogMVC.Data.Extensions
+{
+    public static class DataLayerExtensions 
+    {
+        public static IServiceCollection LoadDataLayerExtension(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            return services;
+        }
+    }
+}
--- YoutubeBlogMVC.Web/Program.cs
+++ YoutubeBlogMVC.Web/Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using YoutubeBlogMVC.Data.Context;
+using YoutubeBlogMVC.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.LoadDataLayerExtension(builder.Configuration);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
